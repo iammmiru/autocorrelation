75a63c846da2cef2df77de6cd35c478fc42310b5
iammmiru <mirulee@mail.com>
2022-05-28 00:45:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# How to correctly calculate an autocorrelation function in python
+
+In statistical physics, an autocorrelation function is widely used to understand a system's nonequilibrium behaivor.<br>
+However, the definition of autocorrelation function used in physics is different from the definition of correlation function used in statistics.<br>
+The physicist's definition is,
+$$
+\left<x_n x_0\right>=\frac{1}{m-n}\sum_{i=0}^{m-1} x_{n+i} x_i,
+$$
+where $m$ is the number of elements. Note that $x_i$ becomes $0$ if $i>m-1$.
+In the mean time, the statistician's definition is
+$$
+z_n=\sum_{i=0}^{m-1} x_{n+i} x_i.
+$$
+
+Unfortunately, the physicist's version is not implemented in the popular python packages such as numpy and scipy.
+Naively using scipy.signal.correlate due to its cosmetic similarity, therefore, often leaves physicists puzzled, forcing them to implement their own algorithms.<br>
+Due to a large number of iterations, however, such algorithms are likely to be very slow when directly implemented in python, especially compared to the scipy.signal.correlate function.<br>
+
+Here, we showcase two correct autocorrelation function implementations that use fast algorithms of the scipy library.<br>
+The first method is using the fast Fourier transform (FFT) algorithm (scipy.fftpack). Note that the FFT method by default performs a cyclical convolution, but an autocorrelation is defined as an acyclical convolution.<br>
+So, blindly implementing the FFT method results in an incorrect autucorrelation function. To remedy this, we insert some zeros in the data, namely zero padding, so that the algorithm runs as if it's acyclical. You can see the difference in the figure.<br>
+The second method is to modify scipy.signal.correlate so that it complies with the physicist's definition of autocorrelation function. It involves dividing each entry by the number of data points that are correlated, i.e., we are calculating the average of correlation.
+
+![alt text](https://github.com/iammmiru/autocorrelation/figure.png?raw=true)
---
+++ autocorrelation.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sp
+import scipy.fftpack as fft
+import scipy.integrate as sint
+
+
+def correlation_scipy(time,data):
+    """
+    Calculate autocorrelation using scipy.signal.correlate function.
+    Note that due to the difference in definition, the result is divided by
+    "factor", which is the number of entries that are correlated.
+    """
+    m            = data.shape[0]
+    dt           = time[1]-time[0]
+    if m%2 != 0:
+        factor       = np.arange(m,0,-1)[:m//2+1]
+        correl       = np.zeros(m//2+1)
+        time_out     = time[:m//2+1]
+    else:
+        factor       = np.arange(m,0,-1)[:m//2]
+        correl       = np.zeros(m//2)
+        time_out     = time[:m//2]
+    correl   = sp.correlate(data,data,
+                            mode='same',method='fft')[m//2:]/factor
+    return time_out,correl
+
+
+def correlation_manual(time, data):
+    """
+    Manually calculate autocorrelation.
+    """
+    m            = data.shape[0]
+    correl       = np.zeros_like(data[:m//2])
+    n = correl.shape[0]
+    for i in range(n):
+        if i == 0:
+            correl[i] = np.sum(data*data, axis = 0)/m
+        else:
+            correl[i] = np.sum(data[:-i]*data[i:], axis = 0)/data[:-i].shape[0]
+    return time[:m//2],correl
+
+def correlation_fft(time, data, zero_padding = True):
+    """
+    Calculate autocorrelation using the fast fourier transform (fft).
+    Since the fft assumes the data is cyclical, one needs to pad some zeros into
+    the data, so that the algorithms runs as if it is acyclical. Otherwise, the
+    algorithm returns an incorrect autocorrelation function.
+
+    Note the result is divided by "factor" only when the zero padding is performed.
+
+    """
+    m            = data.shape[0]
+    dt           = time[1]-time[0]
+    data1        = fft.ifftshift(data)
+    factor       = m
+    if zero_padding == True:
+        data1    = np.r_[data1[:m//2]
+                      ,np.zeros_like(data1),data1[m//2:]]
+        factor   = np.arange(m,0,-1)[:m//2]
+    fft_data     = fft.fft(data1)
+    correl_w     = np.abs(fft_data)**2
+    correl       = fft.ifft(correl_w,axis=0).real[:m//2]/factor
+    return time[:m//2],correl
